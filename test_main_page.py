41c1fd2373c01b5cb5d64ac1751228eda7217712
Greyhawk78 <rubtsov78@mail.ru>
2023-12-08 11:43:36 +0300

    test change

--- test_main_page.py
+++ test_main_page.py
@@ -1,26 +1,8 @@
-import pytest
-
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 
 
-def test_exception1():
-    try:
-        browser = webdriver.Chrome()
-        browser.get("http://selenium1py.pythonanywhere.com/")
-        with pytest.raises(NoSuchElementException):
-            browser.find_element(By.CSS_SELECTOR, "button.btn")
-            pytest.fail("Не должно быть кнопки Отправить")
-    finally: 
-        browser.quit()
-
-def test_exception2():
-    try:
-        browser = webdriver.Chrome()
-        browser.get("http://selenium1py.pythonanywhere.com/")
-        with pytest.raises(NoSuchElementException):
-            browser.find_element(By.CSS_SELECTOR, "no_such_button.btn")
-            pytest.fail("Не должно быть кнопки Отправить")
-    finally: 
-        browser.quit()
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
