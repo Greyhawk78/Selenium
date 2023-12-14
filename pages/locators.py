51ebf8acb08c37424cdeb58227923fc4accd3017
Greyhawk78 <rubtsov78@mail.ru>
2023-12-14 20:44:07 +0300

    Basket page skeleton

--- pages/locators.py
+++ pages/locators.py
@@ -9,6 +9,10 @@ class LoginPageLocators():
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
 
 class ProductPageLocators():
     BASKET_FORM = (By.CSS_SELECTOR,'#add_to_basket_form')
--- test_main_page.py
+++ test_main_page.py
@@ -8,7 +8,17 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     page.should_be_login_link()
     page.go_to_login_page()
+
     page1 = LoginPage(browser, browser.current_url)
     page1.should_be_login_url()
     page1.should_be_login_form()
     page1.should_be_register_form()
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+
+    page1.should_not_be_product_in_basket()
+    page1.basket_is_empty()
